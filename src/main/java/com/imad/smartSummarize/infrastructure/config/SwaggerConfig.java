adf045c9ba40df4cca46d9d9ec18877c63afe22a
Imad Iyad <emadeyad063@gmail.com>
2025-12-19 18:40:05 +0200

    Disable Swagger

--- src/main/java/com/imad/smartSummarize/infrastructure/config/SwaggerConfig.java
+++ src/main/java/com/imad/smartSummarize/infrastructure/config/SwaggerConfig.java
@@ -1,15 +1,6 @@
 package com.imad.smartSummarize.infrastructure.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import org.springframework.context.annotation.Bean;
-
-
+/*
 @OpenAPIDefinition(
         info = @Info(title = "SmartSummarize API", version = "1.0")
 )
@@ -18,10 +9,10 @@ import org.springframework.context.annotation.Bean;
         type = SecuritySchemeType.HTTP,
         bearerFormat = "JWT",
         scheme = "bearer"
-)
+)*/
 //@Configuration
 public class SwaggerConfig {
-    @Bean
+    /*@Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new io.swagger.v3.oas.models.info.Info()
@@ -36,5 +27,5 @@ public class SwaggerConfig {
                                         .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
                                         .bearerFormat("JWT")));
-    }
+    }*/
 }
